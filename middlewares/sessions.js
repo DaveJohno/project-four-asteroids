8833878506eb7275725994feacb220a200682402
DaveJohno <88128050+DaveJohno@users.noreply.github.com>
2022-04-12 08:46:37 +1000

    lastest build to push to heroku

--- middlewares/sessions.js
+++ middlewares/sessions.js
@@ -1,19 +1,11 @@
 const session = require("express-session");
-const db = require("../db/db");
-const connectPgSimple = require("connect-pg-simple");
-const pgSession = connectPgSimple(session);
-
-const oneDay = 1000 * 60 * 60 * 24;
 
 const sessions = session({
-  store: new pgSession({
-    pool: db, // Connects to our postgres db
-    createTableIfMissing: true, // Creates a session table in your database (go look at it!)
-  }),
+  key: "user_sid",
   secret: process.env.EXPRESS_SESSION_SECRET_KEY,
-  cookie: { maxAge: oneDay },
-  resave: false, //gets rid of deprecated messages
-  saveUninitialized: false, //gets rid of deprecated pmessages
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 1000 * 60 * 60 * 24 },
 });
 
 module.exports = sessions;
