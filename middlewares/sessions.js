119e9256ef9dcde5b720bcfa2e488b10bd10617c
DaveJohno <88128050+DaveJohno@users.noreply.github.com>
2022-04-11 12:22:21 +1000

    update

--- middlewares/sessions.js
+++ middlewares/sessions.js
@@ -1,11 +1,19 @@
 const session = require("express-session");
+const db = require("../db/db");
+const connectPgSimple = require("connect-pg-simple");
+const pgSession = connectPgSimple(session);
+
+const oneDay = 1000 * 60 * 60 * 24;
 
 const sessions = session({
-  key: "user_id",
-  secret: process.env["EXPRESS_SESSION_SECRET_KEY"],
-  resave: false,
-  saveUninitialized: false,
-  cookie: { maxAge: 1000 * 60 * 60 * 24 },
+  store: new pgSession({
+    pool: db, // Connects to our postgres db
+    createTableIfMissing: true, // Creates a session table in your database (go look at it!)
+  }),
+  secret: process.env.EXPRESS_SESSION_SECRET_KEY,
+  cookie: { maxAge: oneDay },
+  resave: false, //gets rid of deprecated messages
+  saveUninitialized: false, //gets rid of deprecated pmessages
 });
 
 module.exports = sessions;
